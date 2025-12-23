05aa1d6bad5d0cc1b8791b7f43da72b7c95dcf23
rahimediallo <himera.diallo@gmail.com>
2025-12-23 01:42:44 +0100

    fix project structure and move source to repo root

--- tennis-score-kata/src/main/java/com/kata/tennis/application/port/in/PlayMatchUseCase.java
+++
@@ -1,5 +0,0 @@
-package com.kata.tennis.application.port.in;
-
-public interface PlayMatchUseCase {
-    void play(String sequence);
-}
--- tennis-score-kata/src/main/java/com/kata/tennis/application/port/out/ScorePresenter.java
+++
@@ -1,5 +0,0 @@
-package com.kata.tennis.application.port.out;
-
-public interface ScorePresenter {
-    void onScore(String line);
-}
